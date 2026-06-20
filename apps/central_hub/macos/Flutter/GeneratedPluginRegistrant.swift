28e52327f5f2baac11056be212ecd2db14c8ded9
Dr1pa <dr1pa@MacBook-Air-Dr1pa.local>
2026-06-20 12:15:11 +0300

    chore: update generated plugin registrants and lockfiles

--- apps/central_hub/macos/Flutter/GeneratedPluginRegistrant.swift
+++ apps/central_hub/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,7 +11,6 @@ import connectivity_plus
 import file_picker
 import file_selector_macos
 import flutter_local_notifications
-import path_provider_foundation
 import share_plus
 import shared_preferences_foundation
 import soundpool_macos
@@ -26,7 +25,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SwiftSoundpoolPlugin.register(with: registry.registrar(forPlugin: "SwiftSoundpoolPlugin"))
